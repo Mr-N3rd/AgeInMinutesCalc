4df333ec6797cee66979d5cea22c7fbf856e96ee
Leif Sandvar <n3rdvana@outlook.com>
2022-03-23 17:11:08 -0700

    Cleaned up the logic a bit more, got rid of redundant code!

--- app/src/main/java/com/n3rdvana/ageinminutescalc/MainActivity.kt
+++ app/src/main/java/com/n3rdvana/ageinminutescalc/MainActivity.kt
@@ -2,7 +2,6 @@ package com.n3rdvana.ageinminutescalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -13,6 +12,7 @@ import android.app.DatePickerDialog as DatePickerDialog
 class MainActivity : AppCompatActivity() {
         // Assign your imports as private outside the oncreate function, but within the main activity
     private var tvSelectedDate: TextView? = null
+    private var tvMinutes: TextView? = null
     private val cal: Calendar = Calendar.getInstance()
         // Imports should only be able to be imported within a single activity. If they cross activities then they may cause crashes. This is why you declare them here.
 
@@ -22,10 +22,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+
         val datePickerButton:Button = findViewById(R.id.selectDateButton)
         tvSelectedDate = findViewById(R.id.TVselectedDate)
-
-
+        tvMinutes = findViewById(R.id.minutes_since_birth)
+        tvSelectedDate?.text = "${month+1}/${dayOfMonth}/${year}"
 
         datePickerButton.setOnClickListener {
             clickDatePicker()
@@ -33,37 +35,40 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
-
     val year = cal.get(Calendar.YEAR)
     val month = cal.get(Calendar.MONTH)
-    val dayOfWeek = cal.get(Calendar.DAY_OF_MONTH)
+    val dayOfMonth = cal.get(Calendar.DAY_OF_MONTH)
 
    // When you click the button, it will load a Calendar Prompt
-    fun clickDatePicker(){
+   private fun clickDatePicker(){
         DatePickerDialog(this,
-
             // when you launch the date picker Dialog and Set the date ** On Date Set - >
             { _, selectedYear, selectedMonth, selectedDayOfMonth -> // This lambda expression passes the selectedYear, SelectedMonth and selectedDayOfMonth from the above DatePicker launch
-                val selectedDate= "${selectedDayOfMonth}/${selectedMonth+1}/${selectedYear}"
+                val selectedDate= "${selectedMonth+1}/${selectedDayOfMonth}/${selectedYear}"
+                val currentDate = "${month+1}/${dayOfMonth}/${year}"
+
                 tvSelectedDate?.text = selectedDate
+
+                Toast.makeText(this, "$selectedDate, $currentDate", Toast.LENGTH_SHORT).show()
+
                 val sdf = SimpleDateFormat("mm/dd/yy", Locale.CANADA)
+
                 val formattedDate = sdf.parse(selectedDate)
-//                val getCurrentDate = cal.get(Calendar.getInstance())
-                val currentYear = cal.get(Calendar.YEAR)
-                val currentMonth = cal.get(Calendar.MONTH)
-                val currentDayOfMonth = cal.get(Calendar.DAY_OF_MONTH)
-                val currentDate = "${currentYear}/${currentMonth}/${currentDayOfMonth}"
-//                val formattedCurDate = sdf.parse("${Calendar.getInstance()}")
+                val formattedCurDate = sdf.parse(currentDate)
+
                 val selectedDateInMinutes = (formattedDate.time / 60000)
-//                val currentDateInMinutes = (formattedCurDate.time / 60000)
-////                Log.i("selectedDateInMinutes", "$selectedDateInMinutes")
-                Toast.makeText(this, "$selectedDateInMinutes , $currentDate", Toast.LENGTH_SHORT).show()
-//                Log.i("FormattedCurDate", "$formattedCurDate")
+                val currentDateInMinutes = (formattedCurDate.time / 60000)
+
+
+                val calculatedMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                tvMinutes?.text = calculatedMinutes.toString()
+
+
             },
             year,
             month,
-            dayOfWeek).show()
+            dayOfMonth).show()
     }
 
 }
